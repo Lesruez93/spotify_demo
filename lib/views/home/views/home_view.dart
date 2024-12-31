5d658e1202a7dd11a841f4d6ec6d469e20fd9537
lester <lesterrusike@gmail.com>
2024-12-31 13:30:04 +0200

    added  albums grid view

--- lib/views/home/views/home_view.dart
+++ lib/views/home/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../widgets/albums/albums.dart';
 import '../../widgets/search_bar/search_bar.dart';
 import '../controllers/home_controller.dart';
 
@@ -33,19 +34,24 @@ class HomeView extends GetView<HomeController> {
 
                   if (controller.debounce?.isActive ?? false)
                     controller.debounce!.cancel();
-           //Search delay
+                  //Search delay
                   controller.debounce =
                       Timer(const Duration(milliseconds: 200), () {
-                    if (value != null && value.isNotEmpty) {
-                      controller.fetchAlbums(value);
-                      // album index is 0 and artists index is 1
-                      if (controller.selected.value == 1) {
-                        controller.fetchArtist(value);
-                      } else {
-                        controller.fetchAlbums(value);
-                      }
-                    }
-                  });
+                        if (value == '') {
+                          controller.albums.value = [];
+                        }
+
+                        if (value != null && value.isNotEmpty) {
+                          controller.fetchAlbums(value);
+                          // album index is 0 and artists index is 1
+                          if (controller.selected.value == 1) {
+                            controller.fetchArtist(value);
+                          } else {
+                            controller.fetchAlbums(value);
+                          }
+                        }
+                      });
+                  return null;
                 },
                 labelText: "Search Albums, Artist",
                 controller: controller.searchQuery,
@@ -55,7 +61,7 @@ class HomeView extends GetView<HomeController> {
               spacing: 5.0,
               children: List<Widget>.generate(
                 2,
-                (int index) {
+                    (int index) {
                   return ChoiceChip(
                     selectedColor: Colors.green,
                     backgroundColor: Colors.black,
@@ -80,79 +86,7 @@ class HomeView extends GetView<HomeController> {
                 return Center(child: CircularProgressIndicator());
               }
 
-              return GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.75,
-                    crossAxisSpacing: 8.0,
-                    mainAxisSpacing: 8.0,
-                  ),
-                  itemCount: controller.albums.length,
-                  itemBuilder: (context, index) {
-                    final item = controller.albums[index];
-                    return Card(
-                      color: Colors.transparent,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Image.network(
-                              item.images[0].url,
-                              fit: BoxFit.cover,
-                              width: double.infinity,
-                              loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                                if (loadingProgress == null) {
-                                  return child; // The image is fully loaded.
-                                } else {
-                                  return Center(
-                                    child: CircularProgressIndicator(
-                                      value: loadingProgress.expectedTotalBytes != null
-                                          ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-                                          : null, // Show indeterminate if total bytes are unknown.
-                                    ),
-                                  );
-                                }
-                              },
-                            )
-
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              item.name,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                  color: Colors.white,
-                                  overflow: TextOverflow.ellipsis),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              item.artists[0].name,
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey[700],
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              item.releaseDate,
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey[700],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
+              return AlbumGrid(albums: controller.albums);
             }),
           ),
         ]),
