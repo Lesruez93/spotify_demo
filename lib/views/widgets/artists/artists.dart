220ccbf782c40db1cd2672fd8e0851620cf9f67c
lester <lesterrusike@gmail.com>
2024-12-31 13:52:05 +0200

    added artist list

--- lib/views/widgets/artists/artists.dart
+++ lib/views/widgets/artists/artists.dart
@@ -4,94 +4,64 @@ import '../../home/models/album/album.dart';
 
 
 
-class AlbumGrid extends StatelessWidget {
+class ArtistList extends StatelessWidget {
   final List<Album> albums;
 
-  const AlbumGrid({Key? key, required this.albums}) : super(key: key);
+  const ArtistList({Key? key, required this.albums}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 0.75,
-        crossAxisSpacing: 8.0,
-        mainAxisSpacing: 8.0,
-      ),
+    return ListView.builder(
       itemCount: albums.length,
       itemBuilder: (context, index) {
         final item = albums[index];
-        return AlbumCard(album: item);
+        return ArtistCard(album: item);
       },
     );
   }
 }
 
-class AlbumCard extends StatelessWidget {
+class ArtistCard extends StatelessWidget {
   final Album album;
 
-  const AlbumCard({Key? key, required this.album}) : super(key: key);
+  const ArtistCard({Key? key, required this.album}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       color: Colors.transparent,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Image.network(
-              album.images[0].url,
-              fit: BoxFit.cover,
-              width: double.infinity,
-              loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                if (loadingProgress == null) {
-                  return child; // The image is fully loaded.
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-                          : null, // Show indeterminate if total bytes are unknown.
-                    ),
-                  );
-                }
-              },
-            ),
+
+      child:  ListTile(
+        leading: CircleAvatar(
+          child: Image.network(
+            album.images[0].url,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child; // The image is fully loaded.
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                        : null, // Show indeterminate if total bytes are unknown.
+                  ),
+                );
+              }
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              album.name,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: Colors.white,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
+        ),
+        title:     Text(
+          album.name,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: Colors.white,
+            overflow: TextOverflow.ellipsis,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              album.artists[0].name,
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[700],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              album.releaseDate,
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[700],
-              ),
-            ),
-          ),
-        ],
+        ),
+
       ),
     );
   }
